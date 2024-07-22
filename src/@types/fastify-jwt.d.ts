ee5cfe60695b02c7a92f5730ec96677df08600eb
Matheus <matheus.mleall@gmail.com>
2024-07-22 05:48:42 -0300

    feat: added JWT authentication and started end to end testing with vitest

---
+++ src/@types/fastify-jwt.d.ts
@@ -0,0 +1,10 @@
+import "@fastify/jwt";
+import { number, string } from "zod";
+
+declare module "@fastify/jwt" {
+  export interface FastifyJWT {
+    user: {
+      sub: string;
+    };
+  }
+}
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
+import "dotenv/config";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
