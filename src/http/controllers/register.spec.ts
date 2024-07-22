40a336bf42e0a28f55c55525fa2b3ea03c356103
Matheus <matheus.mleall@gmail.com>
2024-07-22 07:07:55 -0300

    feat: installed supertest and created first e2e tests, setted a way to create and delete a new database schema to e2e tests

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,20 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {});
+describe("Register e2e test", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+  it("It should be able to register", async () => {
+    const response = await request(app.server)
+      .post("/users")
+      .send({ name: "John Doe", email: "john@acme.com", password: "test1234" });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
